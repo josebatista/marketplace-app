d6f6552ade2379d7ca30c1dbf7d6723bea6bfb72
Jose Pereira <josebatistapereira@gmail.com>
2025-02-23 17:05:05 -0300

    ajuste dependencias

--- features/search/presentation/build.gradle.kts
+++ features/search/presentation/build.gradle.kts
@@ -11,10 +11,13 @@ dependencies {
     implementation(projects.core.ui)
     implementation(projects.core.domain)
 
+    implementation(projects.features.search.data)
     implementation(projects.features.search.domain)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
