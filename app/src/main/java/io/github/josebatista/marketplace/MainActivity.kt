bc8738786f27efbb361184d3dbbdb7c533908bbd
José Batista de A. Pereira <josebatistapereira@gmail.com>
2025-02-25 11:50:38 -0300

    criado observer para validar conexao (#7)

--- app/src/main/java/io/github/josebatista/marketplace/MainActivity.kt
+++ app/src/main/java/io/github/josebatista/marketplace/MainActivity.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,6 +28,7 @@ import io.github.josebatista.marketplace.presentation.route.Route
 import io.github.josebatista.marketplace.presentation.theme.MarketplaceAppTheme
 import io.github.josebatista.marketplace.search.presentation.AdaptiveListDetailPanel
 import io.github.josebatista.marketplace.search.presentation.search.SearchScreen
+import io.github.josebatista.marketplace.util.AndroidConnectivityObserver
 
 @AndroidEntryPoint
 public class MainActivity : ComponentActivity() {
@@ -25,25 +37,51 @@ public class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MarketplaceAppTheme {
+                val viewModel = viewModel<MainActivityViewModel> {
+                    MainActivityViewModel(
+                        connectivityObserver = AndroidConnectivityObserver(
+                            context = applicationContext
+                        )
+                    )
+                }
+
+                val isConnected by viewModel.isConnected.collectAsStateWithLifecycle()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val navController = rememberNavController()
-                    NavHost(
-                        modifier = Modifier.padding(innerPadding),
-                        navController = navController,
-                        startDestination = Route.SearchRoute,
-                    ) {
-                        composable<Route.SearchRoute> {
-                            SearchScreen { query ->
-                                navController.navigate(
-                                    Route.ListScreen(
-                                        query = query
-                                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        if (!isConnected) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Red)
+                                    .padding(vertical = 8.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    text = "Sem conexão com a internet",
+                                    color = Color.White
                                 )
                             }
                         }
-                        composable<Route.ListScreen> { backStackEntry ->
-                            val route = backStackEntry.toRoute<Route.ListScreen>()
-                            AdaptiveListDetailPanel(query = route.query)
+                        val navController = rememberNavController()
+                        NavHost(
+                            modifier = Modifier.fillMaxSize(),
+                            navController = navController,
+                            startDestination = Route.SearchRoute,
+                        ) {
+                            composable<Route.SearchRoute> {
+                                SearchScreen { query ->
+                                    navController.navigate(
+                                        Route.ListScreen(
+                                            query = query
+                                        )
+                                    )
+                                }
+                            }
+                            composable<Route.ListScreen> { backStackEntry ->
+                                val route = backStackEntry.toRoute<Route.ListScreen>()
+                                AdaptiveListDetailPanel(query = route.query)
+                            }
                         }
                     }
                 }
